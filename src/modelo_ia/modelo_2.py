1f05ce7e69293305eaea540e99c6331da2bd88d5
Pabl0705 <pabloruzteprograma@gmail.com>
2025-11-20 19:58:39 +0100

    fix: format modelo_2.py

--- src/modelo_ia/modelo_2.py
+++ src/modelo_ia/modelo_2.py
@@ -7,7 +7,8 @@ from sklearn.model_selection import GroupShuffleSplit
 from sklearn.metrics import mean_squared_error, r2_score
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-import os, mlflow
+import os 
+import mlflow
 import matplotlib.pyplot as plt
 
 
