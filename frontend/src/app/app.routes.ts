191d362c1d8a19d1985d8ec3461f3030e3eeb8aa
Pabl0705 <pabloruzteprograma@gmail.com>
2025-12-17 14:39:57 +0100

    feat: updated app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home_module/home/home';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
--- frontend/src/app/home_module/home_module.ts
+++ frontend/src/app/home_module/home_module.ts
@@ -6,7 +6,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home';
 
 @NgModule({
-  declarations: [HomeComponent],
   imports: [
     CommonModule,
     FormsModule,
